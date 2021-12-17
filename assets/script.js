abdafbe4576f2f56516f6ff0874756686d50b206
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2021-12-17 17:35:17 -0600

    added timer decrease on incorrect answer

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,14 @@
 var buttonEl = document.querySelector("#start-button");
 var buttonfirstQuestionEl = document.querySelector("#first-question-button");
 var timerEl = document.querySelector("#time-left");
-
-var selection = document.querySelector("#option");
-var question = 0;
-var quizBank = [
-    ["What object runs code continously until an exit condition is met?", "Loop", "Array","Function","Object"],
-    ["Inside which HTML element do we put JavaScript?","javascript",
-    "js","scripting","script"]
-    ];
-
+var scorce = 0;
+// removes intro text
 function removeIntro(){
     document.getElementById('quiz-overview').style.display = 'none';
     buttonEl.remove();
 }
 
+//starts timer
 var timer = 120;
 function countdown(){
 
@@ -30,27 +24,26 @@ function countdown(){
     }, 1000);
 }
 
+// kicks off first question
 function firstQuestion(){
     document.getElementById('question-1').style.display = 'block';
 
 }
 
-// function displayQuestions(){
-//     listContainer = document.createElement("div")
-//     listElement = document.createElement("li")
-//     numberOfQuestions = quizBank.length;
-//     for (i=0; i < numberOfQuestions; i++) {
-//         listItem = document.createElement('li');
-//         listItem.innerHTML = quizBank[i];
-//         listElement.appendChild(listItem);
-//         listContainer.appendChild(listElement);
-//         listContainer.className = "quiz";
-//         console.log("test")
-//         console.log(listElement)
-//     }
-// }
+function firstQuestionAnswer(){
+    if (document.getElementById('1-correct').checked){
+        document.getElementById('correct-answer').style.display = 'block';
+        score =+ 10;
+    }
+    else {
+        document.getElementById('incorrect-answer').style.display = 'block';
+        timer = timer - 10;
+        return
+    }
+}
 
+buttonfirstQuestionEl.addEventListener("click", firstQuestionAnswer);
 buttonEl.addEventListener("click", firstQuestion);
-// buttonEl.addEventListener("click", displayQuestions);
 buttonEl.addEventListener("click", countdown);
 buttonEl.addEventListener("click", removeIntro);
+
