ce09fbfac835eed4e4494aded5eaf7d1902cb7d0
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2021-12-17 21:29:28 -0600

    added final questions and score display

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,31 @@
 // kick off
 var buttonEl = document.querySelector("#start-button");
-//first button
+// first button
 var buttonfirstQuestionEl = document.querySelector("#first-question-button");
-//second button
+// second button
 var buttonSecondQuestionEl = document.querySelector("#second-question-button");
-//third button
+// third button
 var buttonThirdQuestionEl = document.querySelector("#third-question-button");
 // forth button
 var buttonForthQuestion = document.querySelector("#forth-question-button");
-//fifth button
-var buttonFifthQuestion = document.querySelector("fifth-question-button");
-
+// fifth button
+var buttonFifthQuestion = document.querySelector("#fifth-question-button");
+// sixth button
+var buttonsixthQuestion = document.querySelector("#six-question-button");
+// seventh button
+var buttonseventhQuestion = document.querySelector("#seventh-question-button");
+// eigth button
+var buttoneigthQuestion = document.querySelector("#eigth-question-button");
 // timer and score
+var scoreEL = document.querySelector("#score-total");
+var totalScoreEl = document.querySelector("#total-score");
 var timerEl = document.querySelector("#time-left");
 var score = 0;
 var questionsLeft = 10;
+var timer = 60;
+var totalScore = 0;
+var pause = false;
+console.log(pause)
 
 // removes intro text
 function removeIntro(){
@@ -23,20 +34,22 @@ function removeIntro(){
 }
 
 //starts timer
-var timer = 60;
-
 function countdown(){
     var timeDecay = setInterval(function (){
-        if (timer > 1){
+        if (timer > 0 && pause == false){
             timerEl.textContent = timer;
             timer--;
             timerEl.textContent = timer + " seconds ";
-        } 
+        }
+        // if (timer < 0){
+        //     timerEl.textContent = "0" + " seconds ";
+        // } 
         else {
             timerEl.textContent = "0";
             clearInterval(countdown);
         }
     }, 1000);
+    
 }
 
 // kicks off first question
@@ -46,26 +59,64 @@ function firstQuestion(event){
 }
 // kicks off second question
 function secondQuestion(){
+    event.preventDefault();
     document.getElementById('question-1').style.display = 'none';
     document.getElementById('question-2').style.display = 'block';
 }
 // kicks off third question
 function thirdQuestion(){
+    event.preventDefault();
     document.getElementById('question-2').style.display = 'none';
     document.getElementById('question-3').style.display = 'block';
 }
 // kicks off forth question
 function forthQuestion(){
+    event.preventDefault();
     document.getElementById('question-3').style.display = 'none';
     document.getElementById('question-4').style.display = 'block';
 }
 // kicks off fifth question
 function fifthQuestion(){
+    event.preventDefault();
     document.getElementById('question-4').style.display = 'none';
     document.getElementById('question-5').style.display = 'block';
 }
+//kicks of sixth question
+function sixthQuestion(){
+    event.preventDefault();
+    document.getElementById('question-5').style.display = 'none';
+    document.getElementById('question-6').style.display = 'block';
+}
+//kicks of seventh question
+function seventhQuestion(){
+    event.preventDefault();
+    document.getElementById('question-6').style.display = 'none';
+    document.getElementById('question-7').style.display = 'block';
+}
+//kicks of eigth question
+function eigthQuestion(){
+    event.preventDefault();
+    document.getElementById('question-7').style.display = 'none';
+    document.getElementById('question-8').style.display = 'block';
+}
 
+// Score
+function scoreTotal(){
+    document.getElementById('question-8').style.display = 'none';
+    document.getElementById('score-total').style.display = 'block';
+    document.getElementById('incorrect-answer').style.display = 'none';
+    document.getElementById('correct-answer').style.display = 'none';
+    // timerEl.textContent = timer;
+    totalScore = timer + score;
+    timerEl.textContent = timer;
+    scoreEL.textContent = ("Score from correct answers " + score + " Score from time remaining " + timer + " Total score = " + totalScore );
+    return
+}
+
+
+//submitted answers for each question
 function firstQuestionAnswer(){
+    console.log(timer);
     if (document.getElementById('1-correct').checked){
         document.getElementById('correct-answer').style.display = 'block';
         score = score + 10;
@@ -79,6 +130,7 @@ function firstQuestionAnswer(){
 }
 
 function secondQuestionAnswer(){
+    console.log(timer);
     if (document.getElementById('2-correct').checked){
         document.getElementById('correct-answer').style.display = 'block';
         document.getElementById('incorrect-answer').style.display = 'none';
@@ -95,7 +147,7 @@ function secondQuestionAnswer(){
 }
 
 function thirdQuestionAnswer(){
-    // questionsLeft = questionsLeft - 10;
+    console.log(timer);
     console.log(questionsLeft);
     if (document.getElementById('3-correct').checked){
         document.getElementById('correct-answer').style.display = 'block';
@@ -113,7 +165,7 @@ function thirdQuestionAnswer(){
 }
 
 function forthQuestionAnswer(){
-    
+    console.log(timer);
     console.log(questionsLeft);
     if (document.getElementById('4-correct').checked){
         document.getElementById('correct-answer').style.display = 'block';
@@ -131,7 +183,7 @@ function forthQuestionAnswer(){
 }
 
 function fifthQuestionAnswer(){
-    
+    console.log(timer);
     console.log(questionsLeft);
     if (document.getElementById('5-correct').checked){
         document.getElementById('correct-answer').style.display = 'block';
@@ -148,14 +200,69 @@ function fifthQuestionAnswer(){
     }
 }
 
+function sixthQuestionAnswer(){
+    console.log(timer);
+    console.log(questionsLeft);
+    if (document.getElementById('6-correct').checked){
+        document.getElementById('correct-answer').style.display = 'block';
+        document.getElementById('incorrect-answer').style.display = 'none';
+        score = score + 10;
+        console.log(score);
+        return
+    }
+    else {
+        document.getElementById('incorrect-answer').style.display = 'block';
+        document.getElementById('correct-answer').style.display = 'none';
+        timer = timer - 10;
+        return
+    }
+}
 
-//Start
+function seventhQuestionAnswer(){
+    
+    console.log(questionsLeft);
+    console.log(timer);
+    if (document.getElementById('7-correct').checked){
+        document.getElementById('correct-answer').style.display = 'block';
+        document.getElementById('incorrect-answer').style.display = 'none';
+        score = score + 10;
+        console.log(score);
+        return
+    }
+    else {
+        document.getElementById('incorrect-answer').style.display = 'block';
+        document.getElementById('correct-answer').style.display = 'none';
+        timer = timer - 10;
+        return
+    }
+}
+
+function eigthQuestionAnswer(){
+    questionsLeft = questionsLeft - 10;
+    console.log(timer);
+    if (document.getElementById('8-correct').checked){
+        document.getElementById('correct-answer').style.display = 'block';
+        document.getElementById('incorrect-answer').style.display = 'none';
+        score = score + 10;
+        console.log(score);
+        return
+    }
+    else {
+        document.getElementById('incorrect-answer').style.display = 'block';
+        document.getElementById('correct-answer').style.display = 'none';
+        timer = timer - 10;
+        return
+    }
+}
+
+
+// Start
 buttonEl.addEventListener("click", firstQuestion);
 buttonEl.addEventListener("click", countdown);
 buttonEl.addEventListener("click", removeIntro);
 
 
-//First
+// First
 buttonfirstQuestionEl.addEventListener("click", firstQuestionAnswer);
 buttonfirstQuestionEl.addEventListener("click", secondQuestion);
 
@@ -164,7 +271,7 @@ buttonfirstQuestionEl.addEventListener("click", secondQuestion);
 buttonSecondQuestionEl.addEventListener("click", secondQuestionAnswer);
 buttonSecondQuestionEl.addEventListener("click", thirdQuestion);
 
-//third
+// third
 buttonThirdQuestionEl.addEventListener("click", thirdQuestionAnswer);
 buttonThirdQuestionEl.addEventListener("click", forthQuestion);
 
@@ -172,4 +279,18 @@ buttonThirdQuestionEl.addEventListener("click", forthQuestion);
 buttonForthQuestion.addEventListener("click", forthQuestionAnswer);
 buttonForthQuestion.addEventListener("click", fifthQuestion);
 
-//fifth
+// fifth
+buttonFifthQuestion.addEventListener("click", fifthQuestionAnswer);
+buttonFifthQuestion.addEventListener("click", sixthQuestion);
+
+// sixth
+buttonsixthQuestion.addEventListener("click", sixthQuestionAnswer);
+buttonsixthQuestion.addEventListener("click", seventhQuestion);
+
+//seventh
+buttonseventhQuestion.addEventListener("click", seventhQuestionAnswer);
+buttonseventhQuestion.addEventListener("click", eigthQuestion);
+
+//eigth
+buttoneigthQuestion.addEventListener("click", eigthQuestionAnswer);
+buttoneigthQuestion.addEventListener("click", scoreTotal);
